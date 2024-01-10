cd7a09315515b86d7a048bac1994ee7b4435ec3d
DiegoLimaSoares <dimaslsoares@gmail.com>
2024-01-09 21:26:43 -0300

    Componentes dinâmicos

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -25,6 +25,23 @@ const Subtitulo = styled.h3`
   margin-bottom: 40px;
 `;
 
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  p {
+    width: 200px;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
 function Pesquisa() {
   const [livrosPesquisados, setlivrosPesquisados] = useState([]);
 
@@ -42,6 +59,12 @@ function Pesquisa() {
           setlivrosPesquisados(resultadoPesquisa);
         }}
       />
+      {livrosPesquisados.map((livro) => (
+        <Resultado>
+          <p>{livro.nome}</p>
+          <img src={livro.src} alt="Imagem do livro" />
+        </Resultado>
+      ))}
     </PesquisaContainer>
   );
 }
